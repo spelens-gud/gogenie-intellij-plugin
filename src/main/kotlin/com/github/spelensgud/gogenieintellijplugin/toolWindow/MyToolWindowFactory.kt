72d07a5144b17c39c7968316937d444c3372f028
GitHub Action <action@github.com>
2026-02-26 16:14:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/spelensgud/gogenieintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/spelensgud/gogenieintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.spelensgud.gogenieintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.spelensgud.gogenieintellijplugin.MyBundle
+import com.github.spelensgud.gogenieintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
